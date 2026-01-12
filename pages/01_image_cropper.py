53c41fb29a3e071e7a447b289f3ddb37a4b2cd43
gelmettigeison-jpg <gelmetti.geison@gmail.com>
2026-01-12 11:01:27 +0100

    gittest

--- pages/01_image_cropper.py
+++ pages/01_image_cropper.py
@@ -24,7 +24,7 @@ img = st.file_uploader("Upload an image:", type=["png", "jpg", "jpeg"])
 
 if img is None:
     is_example = True
-    with Image.open("data/starry_night.png") as img:
+    with Image.open("Life_pic.png") as img:
         img_arr = np.array(img)
 else:
     with Image.open(img) as img:
